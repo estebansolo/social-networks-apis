38636eecb110e820fee6d28eec2b4a0ae9bdf886
Esteban Solorzano <estebansolorzano27@gmail.com>
2022-04-11 14:49:54 +0200

    Twitter boilerplate fixed

--- src/providers/twitter/index.js
+++ src/providers/twitter/index.js
@@ -1,11 +1,13 @@
 import express from 'express'
-import linkedinAuthRouter from 'linkedin/api/auth'
+import twitterAuthRouter from 'twitter/routes/auth'
+import twitterApiRouter from 'twitter/routes/api'
 
 
 const router = express.Router()
 
 
-router.use('/auth', linkedinAuthRouter)
+router.use('/auth', twitterAuthRouter)
+router.use('/api', twitterApiRouter)
 
 
 export default router
